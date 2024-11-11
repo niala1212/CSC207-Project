f1c92e119038f05669206c631ac3776db5969d98
Andrew Mei <andrew.mei@mail.utoronto.ca>
2024-11-11 17:44:35 -0500

    gui added

--- src/app/gui/Application.java
+++ src/app/gui/Application.java
@@ -1,8 +1,25 @@
 package app.gui;
 
+import javax.swing.*;
+
 /**
  * GUI class to run the GUI for the entire Application.
  */
 public class Application {
+    static final int WIDTH = 600;
+    static final int HEIGHT = 600;
+
+    /**
+     * Main method to run the GUI.
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame frame = new SearchBox(WIDTH, HEIGHT);
+
+            }
+        });
+    }
 
 }
