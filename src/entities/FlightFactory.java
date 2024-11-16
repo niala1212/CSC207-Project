b1db125903da56ef30179bec82ec60c8054f7418
Daniel-Rogo <daniel.r2017@icloud.com>
2024-11-16 18:33:01 -0500

    Fixed the code to follow comp sci rules

--- src/entities/FlightFactory.java
+++ src/entities/FlightFactory.java
@@ -1,6 +1,6 @@
 package entities;
 
-import data_access.Call_API;
+import java.util.List;
 
 /**
  * Factory for creating Flight objects.
@@ -8,9 +8,32 @@ import data_access.Call_API;
 
 public class FlightFactory {
 
-    public Flight create(String flightNumber, String departureDate){
-        Flight flight = new Flight(flightNumber, departureDate);
-        Call_API.callAPI(flight);
+    /**
+     *
+     * @param flightInfo given as an array with the following format:
+*             [flightNumber, flightDate, Airline, Dep_Airport,
+     *         Arr_Airport, Status, departureScheduled, departureEstimated,
+     *         arrivalScheduled, arrivalEstimated]
+     * @param coordinates given as <latitude, longitude>
+     * @return Populated flight class
+     */
+
+    public static Flight create(List<String> flightInfo, double[] coordinates) {
+        Flight flight = new Flight(flightInfo.get(0), flightInfo.get(1));
+
+        if (coordinates != null) {
+            flight.setCurrentLocation(coordinates);
+        }
+
+        flight.setAirline(flightInfo.get(2));
+        flight.setDepartureAirport(flightInfo.get(3));
+        flight.setArrivalAirport(flightInfo.get(4));
+        flight.setStatus(flightInfo.get(5));
+        flight.setScheduledDepartureTime(flightInfo.get(6));
+        flight.setEstimatedDepartureTime(flightInfo.get(7));
+        flight.setScheduledArrivalTime(flightInfo.get(8));
+        flight.setEstimatedArrivalTime(flightInfo.get(9));
+
         return flight;
     }
 }
