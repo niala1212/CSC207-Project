0fa8e3f8c0bb37b5c1fbfae1dd91f8dbb1e62ab1
Andrew Mei <andrew.mei@mail.utoronto.ca>
2024-11-18 19:14:41 -0500

    added menu and search by flight GUIs

--- src/app/gui/Application.java
+++ src/app/gui/Application.java
@@ -6,9 +6,6 @@ import javax.swing.*;
  * GUI class to run the GUI for the entire Application.
  */
 public class Application {
-    static final int WIDTH = 600;
-    static final int HEIGHT = 600;
-
     /**
      * Main method to run the GUI.
      * @param args Command line arguments.
@@ -16,7 +13,7 @@ public class Application {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                JFrame frame = new SearchBox(WIDTH, HEIGHT);
+                JFrame menu = new Menu();
 
             }
         });
