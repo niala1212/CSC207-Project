e3364cc7d36a70e36e7f850180575a83bfc1340e
Anthonybonev <tony@Anthonys-MacBook-Air-4.local>
2024-11-27 18:08:53 -0500

    Search by Departures state implemented

--- src/java/adapters/SearchByDepartureAirport/SearchByDepartureAirportState.java
+++ src/java/adapters/SearchByDepartureAirport/SearchByDepartureAirportState.java
@@ -1,6 +1,67 @@
 package adapters.SearchByDepartureAirport;
 
 import adapters.AbstractState;
+import entities.Flight;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The state for Search Departures by Airport view model
+ */
 public class SearchByDepartureAirportState extends AbstractState {
+    private String airportName = "";
+    private List<String> flightNumbers = new ArrayList<>();
+    private List<Flight> flights = new ArrayList<>();  // Full flight data
+    private String message = null;
+
+    public String getAirportName() {
+        return airportName;
+    }
+
+    public List<String> getFlightNumbers() {
+        return flightNumbers;
+    }
+
+    public List<Flight> getFlights() {
+        return flights;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setAirportName(String airportName) {
+        this.airportName = airportName;
+    }
+
+    public void setFlightNumbers(List<String> flightNumbers) {
+        this.flightNumbers = flightNumbers;
+    }
+
+    public void setFlights(List<Flight> flights) {
+        this.flights = flights;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSuccessful() {
+        return message == null;
+    }
+
+    /**
+     * The to-string to display all the flights
+     * @param flightNumber flight number of flight
+     * @return string of all the flight details
+     */
+    public String getFlightDetailsString(String flightNumber) {
+        for (Flight flight : flights) {
+            if (flight.getFlightNumber().equals(flightNumber)) {
+                return flight.toString(); // Format this string as needed
+            }
+        }
+        return null; // Return null if no flight is found
+    }
 }
